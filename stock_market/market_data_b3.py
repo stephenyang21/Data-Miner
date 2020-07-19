4637f8a6ad68e8fd554de0fc4b008e1b71cb1c8b
Stephen Zi Chyi Lin Yang <StephenYang21@Stephens-MacBook-Pro.local>
2020-07-18 21:37:40 -0300

    adding new feature

--- stock_market/market_data_b3.py
+++ stock_market/market_data_b3.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import numpy as np
+import datetime
 
 from io import BytesIO
 from zipfile import ZipFile
@@ -8,11 +9,13 @@ from urllib.request import urlopen
 
 
 class IntraDayPriceB3:
-    def __init__(self,  date, stock=None, derivative=None):
+    def __init__(self,  date, stock=True, derivative=None):
         self.stock = stock
         self.derivative = derivative
         self.date = date
         self.url = 'https://arquivos.b3.com.br/apinegocios/tickercsv/{}'
+
+        self.df = None
 
     def _load_data(self):
         self.url = self.url.format(self.date)
@@ -20,6 +23,19 @@ class IntraDayPriceB3:
         _zipfile = ZipFile(BytesIO(_data.read()))
 
         with _zipfile.open(_zipfile.namelist()[0], 'r') as g:
-            df = pd.read_csv(g, sep=';')
+            self.df = pd.read_csv(g, sep=';')
 
-        return df
+        if self.stock:
+            self.df = self.df[(self.df .TckrSymb.str.len() == 5)]
+        return self.df
+
+    def get_data(self, ticker):
+        self.df = self.df[["RptDt", "TckrSymb", "GrssTradAmt",
+                           "TradQty", "NtryTm", "TradId", "TradDt"]]
+        self.df.columns = ["report_date", "ticker", "price",
+                           "trade_quatity", "entry_time", "trade_id", "trade_date"]
+        self.df['price'] = (
+            self.df['price'].str.replace(',', '.')).astype(float)
+
+        self.df = self.df[self.df["ticker"] == ticker]
+        return self.df
